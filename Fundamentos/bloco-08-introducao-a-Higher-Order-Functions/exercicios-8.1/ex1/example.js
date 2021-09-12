1c380f614f8ce75bb4a50d82afe6076b63cd2995
Andre Luiz <andreluizgyn1@gmail.com>
2021-09-12 16:16:39 -0300

    Exercícios do dia 8.2

--- Fundamentos/bloco-08-introducao-a-Higher-Order-Functions/exercicios-8.1/ex1/example.js
+++
@@ -1,9 +0,0 @@
-const wakeUp = () => 'Acordando!!';
-const breakFast = () => 'Bora tomar café!!';
-const sleep = () => 'Partiu dormir!!';
-
-const doingThings = (thing) => console.log(thing());
-
-doingThings(wakeUp);
-doingThings(breakFast);
-doingThings(sleep);
---
+++ Fundamentos/bloco-08-introducao-a-Higher-Order-Functions/exercise7.js
@@ -0,0 +1,86 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expectedResult = false;
+
+function test(book) {
+  let x = 0
+  for (let index = 0; index < books.length; index += 1) {
+    if (book.author.birthYear === books[index].author.birthYear) {
+      x += 1;
+    }
+  }
+  
+  const result = x > 1 ? false : true;
+  return result;
+}
+
+function authorUnique() {
+  const tests = books.map(test)
+  const result = tests.every((test) => test === true)
+  return result
+}
+
+assert.strictEqual(authorUnique(), expectedResult);
